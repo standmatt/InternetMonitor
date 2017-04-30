4e70d25fe2fd4e382925fab80ec137a3c97ec715
Matthew Standiford <matt@Matthews-MacBook-Pro.local>
2017-04-30 00:34:34 -0400

    Updated Plotting

--- testPlot.py
+++ testPlot.py
@@ -1,29 +1,66 @@
 import numpy as np
 
-from bokeh.layouts import gridplot
+from bokeh.layouts import column
 from bokeh.plotting import figure, save, output_file
-from CodernityDB.database import Database
-
-#Open file
-#Constants
-dbfile = '/opt/internetmonitor/data/imdb.sqlite'
-db = Database(dbfile)
-db.open()
-
-for curr in db.all('id'):
-	print curr['self']
+from bokeh.models import DatetimeTickFormatter
+import sqlite3
+import datetime
+from datetime import datetime
 
 def datetime(x):
-    return np.array(x, dtype=np.datetime64)
+    return np.array(x, dtype='datetime64[ms]')
+
+#Constants
+dbfile = 'data/imdb.sqlite'
+dbconn=sqlite3.connect(dbfile)
+db=dbconn.cursor()
+
+db.execute('''SELECT datetime,upload,download,status from InternetStatus''')
+time = db.fetchall()
+
+timestamp,upload,download,status = (
+  [int((l[0]-14400)*1000) for l in time], 
+  [l[1]/1000000 for l in time],
+  [l[2]/1000000 for l in time],
+  [l[3] for l in time]
+)
+
+print(timestamp)
 
 p1 = figure(x_axis_type="datetime", title="Internet Speed")
+#p1 = figure(title="Internet Speed")
+
+p1.xaxis.formatter=DatetimeTickFormatter(formats=dict(
+    minutes=["%d %B %Y"],
+    hours=["%d %b %Y"],
+    days=["%d %b %Y"],
+    months=["%d %b %Y"],
+    years=["%d %b %Y"]))
+
 p1.grid.grid_line_alpha=0.3
 p1.xaxis.axis_label = 'Date'
 p1.yaxis.axis_label = 'Internet Speed'
 
-p1.line(datetime(AAPL['date']), AAPL['adj_close'], color='#A6CEE3', legend='Upload')
-p1.line(datetime(GOOG['date']), GOOG['adj_close'], color='#B2DF8A', legend='Download')
+p1.line(datetime(timestamp), upload, color='Red', legend='Upload')
+p1.line(datetime(timestamp), download, color='Blue', legend='Download')
 p1.legend.location = "top_left"
 
-output_file("/opt/internetmonitor/stocks.html", title="stocks.py example")
-save(gridplot([[p1,p2]], plot_width=400, plot_height=400))
+p2 = figure (x_axis_type="datetime", title="Internet Up/Down")
+p2.xaxis.formatter=DatetimeTickFormatter(formats=dict(
+    minutes=["%d %B %Y"],
+    hours=["%d %b %Y"],
+    days=["%d %b %Y"],
+    months=["%d %b %Y"],
+    years=["%d %b %Y"]))
+
+p2.grid.grid_line_alpha=0.3
+p2.xaxis.axis_label = 'Date'
+p2.yaxis.axis_label = 'Internet Status'
+
+p2.line(datetime(timestamp), status, color='Red', legend='Status')
+p2.legend.location = "top_left"
+
+
+output_file("stocks.html", title="stocks.py example")
+p=column(p1,p2)
+save(p)
